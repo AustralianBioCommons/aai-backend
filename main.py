efe89650e927cb6f7ed400d20389a57e0897cf49
Minh Vu <minh@biocommons.org.au>
2025-04-24 15:38:58 +1000

    Fix indentation in main.py for consistency

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ app = FastAPI()
 
 @app.get("/")
 def public_route():
-    return {"message": "Public route"}
+	return {"message": "Public route"}
 
 
 @app.get("/private")
 def private_route(user=Depends(get_current_user)):
-    return {
-        "message": "Private route",
-        "user_claims": user,
-        "management_token": get_management_token()
-    }
+	return {
+		"message": "Private route",
+		"user_claims": user,
+		"management_token": get_management_token()
+	}
 
 
 app.include_router(user.router)
