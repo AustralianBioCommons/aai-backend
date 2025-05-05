ba8d5a67f0dd851ad1403b287e113a36b37c2bce
marius-mather <marius.mather@sydney.edu.au>
2025-05-05 11:38:35 +1000

    CDK stack to deploy app

---
+++ deploy/aai_backend_deploy/aai_backend_deploy_stack.py
@@ -0,0 +1,68 @@
+from aws_cdk import (
+    Stack, CfnOutput,
+    aws_ec2 as ec2,
+    aws_ecs as ecs,
+    aws_ecr as ecr,
+    aws_iam as iam,
+    aws_route53 as route53,
+    aws_certificatemanager as acm,
+    aws_elasticloadbalancingv2 as elbv2,
+    aws_ecs_patterns as ecs_patterns,
+)
+from constructs import Construct
+
+
+class AaiBackendDeployStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, config: dict, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        self.certificate_arn = config["AWS_CERTIFICATE_ARN"]
+        self.zone_id = config["AWS_ZONE_ID"]
+        self.zone_domain = config["AWS_ZONE_DOMAIN"]
+
+        # VPC
+        vpc = ec2.Vpc(self, "AaiBackendVPC", max_azs=2)
+
+        # ECS Cluster
+        cluster = ecs.Cluster(self, "AaiBackendCluster", vpc=vpc)
+
+        # Reference the existing ECR repository
+        ecr_repo = ecr.Repository.from_repository_name(self, "AaiBackendRepo", "aai-backend")
+
+        # Task definition for Fargate
+        task_definition = ecs.FargateTaskDefinition(self, "AaiBackendTaskDef",
+                                                    memory_limit_mib=1024,
+                                                    cpu=512)
+
+        container = task_definition.add_container("FastAPIContainer",
+            image=ecs.ContainerImage.from_ecr_repository(ecr_repo, tag="latest"),
+            logging=ecs.LogDrivers.aws_logs(stream_prefix="FastAPI"),
+        )
+
+        container.add_port_mappings(
+            ecs.PortMapping(container_port=8000)
+        )
+
+        # HTTPS Certificate
+        certificate = acm.Certificate.from_certificate_arn(self, "BackendCert", self.certificate_arn)
+
+        # Create ALB + Fargate Service
+        service = ecs_patterns.ApplicationLoadBalancedFargateService(
+            self, "AaiBackendService",
+            cluster=cluster,
+            task_definition=task_definition,
+            public_load_balancer=True,
+            listener_port=443,
+            protocol=elbv2.ApplicationProtocol.HTTPS,
+            certificate=certificate,
+            redirect_http=True,
+            domain_name="aaibackend." + self.zone_domain,
+            domain_zone=route53.HostedZone.from_lookup(
+                self, "AaiBackendZone", domain_name=self.zone_domain
+            ),
+        )
+
+        service.target_group.configure_health_check(path="/", healthy_http_codes="200-399")
+
+        CfnOutput(self, "LoadBalancerDNS", value=service.load_balancer.load_balancer_dns_name)
