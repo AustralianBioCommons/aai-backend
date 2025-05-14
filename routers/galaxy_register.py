3d4c41453d039182a5bed7dfdcbbe679e7f6c196
marius-mather <marius.mather@sydney.edu.au>
2025-05-14 14:05:41 +1000

    Registration endpoints

---
+++ routers/galaxy_register.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+import httpx
+from fastapi import APIRouter, Header, HTTPException
+from fastapi.params import Depends
+
+from auth.config import get_settings, Settings
+from auth.management import get_management_token
+from register.tokens import create_registration_token, verify_registration_token
+from schemas.galaxy import GalaxyRegistrationData
+
+router = APIRouter(
+    prefix="/galaxy", tags=["galaxy", "registration"]
+)
+
+
+@router.get("/get-registration-token")
+async def get_registration_token():
+    return {"token": create_registration_token()}
+
+
+@router.post("/register")
+def register(
+        registration_data: GalaxyRegistrationData,
+        registration_token: Optional[str] = Header(None),
+        settings: Settings = Depends(get_settings),
+):
+    if not registration_token:
+        raise HTTPException(status_code=400, detail="Missing registration token")
+
+    verify_registration_token(registration_token)
+
+    url = f"https://{settings.auth0_domain}/api/v2/users/"
+    management_token = get_management_token()
+    headers = {"Authorization": f"Bearer {management_token}"}
+    user_data = registration_data.to_auth0_create_user_data()
+    resp = httpx.post(url, json=user_data.model_dump(), headers=headers)
+    return {"message": "User registered successfully", "user": resp.json()}
+
