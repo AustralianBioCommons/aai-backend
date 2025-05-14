4b2aa5b4b1861b5f651a854490d720ffce45f082
marius-mather <marius.mather@sydney.edu.au>
2025-05-14 16:27:34 +1000

    Rework how settings are used to allow overriding in tests

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
@@ -5,15 +7,26 @@ from auth.config import get_settings
 from routers import user
 from routers import galaxy_register
 
-app = FastAPI()
-settings = get_settings()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=settings.cors_allowed_origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],  # Include 'registration-token' if needed
-)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Set up CORS middleware after startup so we can override it in testing
+    :param app:
+    :return:
+    """
+    settings = app.dependency_overrides.get(get_settings, get_settings)()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.cors_allowed_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+    )
+
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
