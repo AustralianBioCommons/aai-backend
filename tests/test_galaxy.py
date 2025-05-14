d01972e7d8c831cf5fa27edf319e434d4cb3dbd3
marius-mather <marius.mather@sydney.edu.au>
2025-05-14 16:37:52 +1000

    Use settings override to simplify tests

--- tests/test_galaxy.py
+++ tests/test_galaxy.py
@@ -1,53 +1,10 @@
-from contextlib import ExitStack
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock
 
 import pytest
-from fastapi.testclient import TestClient
 from jose import jwt
 
-from auth.config import Settings
-from main import app
-from schemas.galaxy import GalaxyRegistrationData, Auth0CreateUserData, Auth0UserMetadata
+from schemas.galaxy import GalaxyRegistrationData
 from tests.datagen import AccessTokenPayloadFactory, GalaxyRegistrationDataFactory
-
-client = TestClient(app)
-
-
-@pytest.fixture
-def mock_settings():
-    """Fixture that returns mocked Settings object."""
-    return Settings(
-        auth0_domain="mock-domain",
-        auth0_management_id="mock-id",
-        auth0_management_secret="mock-secret",
-        auth0_audience="mock-audience",
-        jwt_secret_key="mock-secret-key",
-        cors_allowed_origins=["http://test"],
-        auth0_algorithms=["HS256"]
-    )
-
-
-@pytest.fixture(autouse=True)
-def patch_get_settings(mock_settings):
-    """Globally patch get_settings() and override in FastAPI dependency injection."""
-    import auth.config  # where get_settings is defined
-
-    auth.config.get_settings.cache_clear()
-
-    # Need to use ExitStack to patch multiple targets
-    patch_targets = [
-        "register.tokens.get_settings",
-        "auth.config.get_settings",
-        "auth.management.get_settings",
-        "main.get_settings",
-    ]
-    with ExitStack() as stack:
-        # Override FastAPI dependency
-        app.dependency_overrides[auth.config.get_settings] = lambda: mock_settings
-        for path in patch_targets:
-            stack.enter_context(patch(path, return_value=mock_settings))
-        yield
-        app.dependency_overrides.clear()
 
 
 @pytest.fixture
@@ -68,11 +25,11 @@ def mock_verify_token(mocker):
     mocker.patch("routers.galaxy_register.verify_registration_token", return_value=None)
 
 
-def test_get_registration_token(mock_settings):
+def test_get_registration_token(client_with_settings_override, mock_settings):
     """
     Test get-registration-token endpoint returns a valid JWT token.
     """
-    response = client.get("/galaxy/get-registration-token")
+    response = client_with_settings_override.get("/galaxy/get-registration-token")
     assert response.status_code == 200
     decoded = jwt.decode(response.json()["token"], mock_settings.jwt_secret_key,
                          algorithms=mock_settings.auth0_algorithms)
@@ -98,7 +55,7 @@ def test_to_auth0_create_user_data_valid():
     assert auth0_data.user_metadata.galaxy_username == "valid_username"
 
 
-def test_register(mocker, mock_auth_token, mock_settings):
+def test_register(mocker, mock_auth_token, mock_settings, client_with_settings_override):
     """
     Try to test our register endpoint. Since we don't want to call
     an actual Auth0 API, test that:
@@ -112,9 +69,9 @@ def test_register(mocker, mock_auth_token, mock_settings):
     mock_resp.status_code = 201
     mock_post = mocker.patch("httpx.post", return_value=mock_resp)
     user_data = GalaxyRegistrationDataFactory.build()
-    token_resp = client.get("/galaxy/get-registration-token")
+    token_resp = client_with_settings_override.get("/galaxy/get-registration-token")
     headers = {"registration-token": token_resp.json()["token"]}
-    resp = client.post("/galaxy/register", json=user_data.model_dump(), headers=headers)
+    resp = client_with_settings_override.post("/galaxy/register", json=user_data.model_dump(), headers=headers)
     assert resp.status_code == 200
     assert resp.json()["message"] == "User registered successfully"
     assert resp.json()["user"] == {"user_id": "abc123"}
