b631404c37e12cc034a5d525871339c6dbb40886
marius-mather <marius.mather@sydney.edu.au>
2025-05-14 14:10:21 +1000

    Add data generation for registration data

--- tests/datagen.py
+++ tests/datagen.py
@@ -1,5 +1,7 @@
+from polyfactory.decorators import post_generated
 from polyfactory.factories.pydantic_factory import ModelFactory
 
+from schemas.galaxy import GalaxyRegistrationData
 from schemas.service import Auth0User
 from schemas.tokens import AccessTokenPayload
 from schemas.user import User
@@ -12,3 +14,14 @@ class UserFactory(ModelFactory[User]): ...
 
 
 class Auth0UserFactory(ModelFactory[Auth0User]): ...
+
+
+class GalaxyRegistrationDataFactory(ModelFactory[GalaxyRegistrationData]):
+
+    @post_generated
+    @classmethod
+    def password_confirmation(cls, password: str) -> str:
+        """
+        Use the same value as password for password_confirmation.
+        """
+        return password
