1bb06cbb0e63dd5a32e7240372b2a988be7a72aa
Minh Vu <minh@biocommons.org.au>
2025-05-19 16:57:20 +1000

    Add BPA registration endpoint and related tests
    - Implemented the BPA registration API endpoint with validation for user input and organization selection.
    - Added settings for organizations in the configuration.
    - Created comprehensive tests for successful registration, error handling, and input validation.

--- auth/config.py
+++ auth/config.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+from typing import Dict, List
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -13,6 +14,26 @@ class Settings(BaseSettings):
     # Note we process this separately in app startup as it needs
     #   to be available before the app starts
     cors_allowed_origins: str
+    organizations: List[Dict[str, str]] = [
+        {"id": "bpa-bioinformatics-workshop", "name": "2024 Fungi Bioinformatics Workshop"},
+        {"id": "cipps", "name": "ARC for Innovations in Peptide and Protein Science (CIPPS)"},
+        {"id": "ausarg", "name": "Australian Amphibian and Reptile Genomics"},
+        {"id": "aus-avian", "name": "Australian Avian Genomics"},
+        {"id": "aus-fish", "name": "Australian Fish Genomics"},
+        {"id": "grasslands", "name": "Australian Grasslands Initiative"},
+        {"id": "fungi", "name": "Fungi Functional 'Omics"},
+        {"id": "forest-resilience", "name": "Genomics for Forest Resilience"},
+        {"id": "bpa-great-barrier-reef", "name": "Great Barrier Reef"},
+        {"id": "bpa-ipm", "name": "Integrated Pest Management 'Omics"},
+        {"id": "bpa-omg", "name": "Oz Mammals Genomics Initiative"},
+        {"id": "plant-pathogen", "name": "Plant Pathogen 'Omics"},
+        {"id": "ppa", "name": "Plant Protein Atlas"},
+        {"id": "australian-microbiome", "name": "The Australian Microbiome Initiative"},
+        {"id": "threatened-species", "name": "Threatened Species Initiative"},
+        {"id": "bpa-wheat-cultivars", "name": "Wheat Cultivars"},
+        {"id": "bpa-wheat-pathogens-genomes", "name": "Wheat Pathogens Genomes"},
+        {"id": "bpa-wheat-pathogens-transcript", "name": "Wheat Pathogens Transcript"},
+    ]
 
     model_config = SettingsConfigDict(env_file=".env")
 
--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from routers import galaxy_register, user
+from routers import bpa_register, galaxy_register, user
 
 # Load .env to get CORS_ALLOWED_ORIGINS.
 # Note that for most env variables, we use pydantic-settings
 #   and load them via auth.config. But we need the
 #   allowed_origins before we load the app
 load_dotenv()
-ALLOWED_ORIGINS = [origin.strip()
-                   for origin in os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")]
+ALLOWED_ORIGINS = [
+    origin.strip() for origin in os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
+]
 
 app = FastAPI()
 app.add_middleware(
@@ -20,7 +21,7 @@ app.add_middleware(
     allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 
@@ -30,4 +31,5 @@ def public_route():
 
 
 app.include_router(user.router)
+app.include_router(bpa_register.router)
 app.include_router(galaxy_register.router)
