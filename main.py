9b984d799c9c2de6d1a86e582ffcf38909e614ec
marius-mather <marius.mather@sydney.edu.au>
2025-05-23 16:00:16 +1000

    Don't load env variables in main, just grab the needed value from the file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
 
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
@@ -10,9 +9,9 @@ from routers import admin, bpa_register, galaxy_register, user
 # Note that for most env variables, we use pydantic-settings
 #   and load them via auth.config. But we need the
 #   allowed_origins before we load the app
-load_dotenv()
+env_values = dotenv_values(".env")
 ALLOWED_ORIGINS = [
-    origin.strip() for origin in os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
+    origin.strip() for origin in env_values.get("CORS_ALLOWED_ORIGINS", "").split(",")
 ]
 
 app = FastAPI()
