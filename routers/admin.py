cd622a8c37e4cf15f7e1e083979188d165d8f4c0
marius-mather <marius.mather@sydney.edu.au>
2025-05-28 14:11:01 +1000

    Update where we use app_metadata in the endpoint

--- routers/admin.py
+++ routers/admin.py
@@ -9,7 +9,6 @@ from auth.validator import get_current_user, user_is_admin
 from auth0.client import Auth0Client
 from auth0.schemas import Auth0UserResponse
 from routers.user import update_user_metadata
-from schemas.service import AppMetadata
 from schemas.user import User
 
 logger = logging.getLogger('uvicorn.error')
@@ -58,14 +57,13 @@ def approve_service(user_id: str = Path(..., pattern=r"^auth0\\|[a-zA-Z0-9]+$"),
                     client: Auth0Client = Depends(get_auth0_client),
                     approving_user: User = Depends(get_current_user)):
     user = client.get_user(user_id=user_id)
-    # NOTE: we currently don't enforce structure on app_metadata for Auth0UserResponse,
-    #   only in our User model, so we need to convert from dict
-    app_metadata = AppMetadata(**user.app_metadata)
+    # Need to fetch full user info currently to get email address, not in access token
     approving_user_data = client.get_user(user_id=approving_user.access_token.sub)
     logger.debug(f"Approving service {service_id} for user {user_id} by {approving_user_data.email}")
-    app_metadata.approve_service(service_id, approved_by=str(approving_user_data.email))
+    user.app_metadata.approve_service(service_id, approved_by=str(approving_user_data.email))
     logger.info("Sending updated metadata to Auth0 API")
-    update = update_user_metadata(user_id=user_id, token=client.management_token, metadata=app_metadata.model_dump(mode="json"))
+    # update_user_metadata is async, so run via asyncio
+    update = update_user_metadata(user_id=user_id, token=client.management_token, metadata=user.app_metadata.model_dump(mode="json"))
     resp = asyncio.run(update)
     logger.info("Metadata updated successfully")
     return resp
