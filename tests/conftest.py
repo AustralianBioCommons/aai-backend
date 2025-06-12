24f79da2a4f4c22bfc4853d08e13198ca42160c5
marius-mather <marius.mather@sydney.edu.au>
2025-06-12 11:56:06 +1000

    Make sure we ignore env file in testing, mock galaxy settings where needed

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ from auth.config import Settings, get_settings
 from auth.management import get_management_token
 from auth.validator import get_current_user
 from galaxy.client import GalaxyClient, get_galaxy_client
+from galaxy.config import GalaxySettings, get_galaxy_settings
 from main import app
 from tests.datagen import AccessTokenPayloadFactory, SessionUserFactory
 
@@ -19,7 +20,10 @@ def ignore_env_file():
     """
     def get_settings_no_env_file():
         return Settings(_env_file=None)
+    def get_galaxy_settings_no_env_file():
+        return GalaxySettings(_env_file=None)
     app.dependency_overrides[get_settings] = get_settings_no_env_file
+    app.dependency_overrides[get_galaxy_settings] = get_galaxy_settings_no_env_file
 
 
 @pytest.fixture
@@ -36,8 +40,15 @@ def mock_settings():
         auth0_algorithms=["HS256"]
     )
 
+
 @pytest.fixture
-def test_client(mock_settings):
+def mock_galaxy_settings():
+    """Dummy settings values for GalaxySettings"""
+    return GalaxySettings(galaxy_url="http://mock-url", galaxy_api_key="mock-key")
+
+
+@pytest.fixture
+def test_client(mock_settings, mock_galaxy_settings):
     """
     Override the get_settings dependency to return a mocked Settings object.
     """
@@ -47,6 +58,7 @@ def test_client(mock_settings):
 
     # Apply override
     app.dependency_overrides[get_settings] = override_settings
+    app.dependency_overrides[get_galaxy_settings] = lambda: mock_galaxy_settings
 
     # Create client
     client = TestClient(app)
