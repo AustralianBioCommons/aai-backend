2cb50d43e6b342929f8b27a75df0faa4787af76f
marius-mather <marius.mather@sydney.edu.au>
2025-07-15 14:29:32 +1000

    datagen utils for database tests

---
+++ tests/db/datagen.py
@@ -0,0 +1,11 @@
+from polyfactory.factories.sqlalchemy_factory import SQLAlchemyFactory
+
+from db.models import Auth0Role, BiocommonsGroup
+
+
+class Auth0RoleFactory(SQLAlchemyFactory[Auth0Role]):
+    __set_relationships__ = True
+
+
+class BiocommonsGroupFactory(SQLAlchemyFactory[BiocommonsGroup]):
+    __set_relationships__ = True
