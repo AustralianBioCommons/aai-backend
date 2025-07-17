d2f219d9c7d127c6e1adf0314de0bc82ef3c722a
marius-mather <marius.mather@sydney.edu.au>
2025-07-17 10:33:15 +1000

    BiocommonsGroupCreate model that doesn't require the DB

--- biocommons/groups.py
+++ biocommons/groups.py
@@ -1,7 +1,16 @@
 import re
+from typing import Annotated
+
+from pydantic import StringConstraints
+from sqlmodel import Session, select
+
+from db.core import BaseModel
+from db.models import Auth0Role, BiocommonsGroup
 
 GroupIdPattern = re.compile(r"^biocommons/group/[a-z0-9_]+$")
+GroupId = Annotated[str, StringConstraints(pattern=GroupIdPattern)]
 RoleIdPattern = re.compile(r"^biocommons/role/[a-z0-9_]+/[a-z0-9_]+$")
+RoleId = Annotated[str, StringConstraints(pattern=RoleIdPattern)]
 
 
 def is_valid_group_id(group_id: str) -> bool:
@@ -10,3 +19,33 @@ def is_valid_group_id(group_id: str) -> bool:
 
 def is_valid_role_id(role_id: str) -> bool:
     return RoleIdPattern.match(role_id) is not None
+
+
+# Note: not using table=True so this is just a data model,
+#   not saved in the DB
+class BiocommonsGroupCreate(BaseModel):
+    """
+    Data needed to create a new biocommons group
+    """
+    group_id: GroupId
+    name: str
+    admin_roles: list[RoleId | Auth0Role]
+
+    # TODO: currently requires the roles to exist in the DB
+    #   already, probably want to get_or_create them
+    def save(self, session: Session):
+        db_roles = []
+        for role in self.admin_roles:
+            if isinstance(role, Auth0Role):
+                db_roles.append(role)
+            else:
+                query = select(Auth0Role).where(Auth0Role.name == role)
+                auth0_role = session.exec(query).one()
+                db_roles.append(auth0_role)
+        group = BiocommonsGroup(
+            group_id=self.group_id,
+            name=self.name,
+            admin_roles=db_roles
+        )
+        session.add(group)
+        session.commit()
--- tests/biocommons/test_groups.py
+++ tests/biocommons/test_groups.py
@@ -1,6 +1,9 @@
 import pytest
+from sqlmodel import select
 
-from biocommons.groups import is_valid_group_id, is_valid_role_id
+from biocommons.groups import BiocommonsGroupCreate, is_valid_group_id, is_valid_role_id
+from db.models import BiocommonsGroup
+from tests.db.datagen import Auth0RoleFactory
 
 
 @pytest.mark.parametrize("group_id", [
@@ -28,3 +31,35 @@ def test_invalid_group_ids(group_id):
 ])
 def test_valid_role_ids(role_id):
     assert is_valid_role_id(role_id)
+
+
+def test_biocommons_group_create():
+    """
+    Test we can create a BiocommonsGroupCreate object
+    that doesn't required the DB
+    """
+    group = BiocommonsGroupCreate(
+        group_id="biocommons/group/tsi",
+        name="Threatened Species Initiative",
+        admin_roles=["biocommons/role/tsi/admin", "biocommons/role/bpa/admin"]
+    )
+    assert group.group_id == "biocommons/group/tsi"
+
+
+def test_biocommons_group_create_save(session):
+    """
+    Test saving BiocommonsGroupCreate object to the DB
+    """
+    tsi_admin = Auth0RoleFactory.build(name="biocommons/role/tsi/admin")
+
+    sysadmin = Auth0RoleFactory.build(name="biocommons/role/biocommons/sysadmin")
+    group = BiocommonsGroupCreate(
+        group_id="biocommons/group/tsi",
+        name="Threatened Species Initiative",
+        admin_roles=[tsi_admin, sysadmin]
+    )
+    group.save(session)
+    group_from_db = session.exec(
+        select(BiocommonsGroup).where(BiocommonsGroup.group_id == group.group_id)
+    ).one()
+    assert group_from_db.group_id == group.group_id
