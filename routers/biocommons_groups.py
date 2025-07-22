033202502a24842add815deac966d52a73d06d47
marius-mather <marius.mather@sydney.edu.au>
2025-07-22 11:52:19 +1000

    API endpoint to create a role

--- routers/biocommons_groups.py
+++ routers/biocommons_groups.py
@@ -3,11 +3,13 @@ from http import HTTPStatus
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
 from sqlmodel import Session
 
 from auth.validator import get_current_user, user_is_admin
 from auth0.client import Auth0Client, get_auth0_client
-from biocommons.groups import BiocommonsGroupCreate, BiocommonsGroupResponse
+from biocommons.groups import BiocommonsGroupCreate, BiocommonsGroupResponse, RoleId
+from db.models import Auth0Role
 from db.setup import get_db_session
 
 logger = logging.getLogger('uvicorn.error')
@@ -23,6 +25,10 @@ def create_group(
         group_info: BiocommonsGroupCreate,
         db_session: Annotated[Session, Depends(get_db_session)],
         auth0_client: Annotated[Auth0Client, Depends(get_auth0_client)]):
+    """
+    Create a new group in the DB. Note that the Auth0 role for this group
+    must already exist.
+    """
     # Check group exists in Auth0
     try:
         auth0_client.get_role_by_name(group_info.name)
@@ -37,3 +43,29 @@ def create_group(
         name=group.name,
         admin_roles=[r.name for r in group.admin_roles]
     )
+
+
+class CreateRoleData(BaseModel):
+    name: RoleId
+    description: str
+
+
+@router.post("/roles/create",
+             dependencies=[Depends(user_is_admin)],)
+def create_role(
+        role_data: CreateRoleData,
+        db_session: Annotated[Session, Depends(get_db_session)],
+        auth0_client: Annotated[Auth0Client, Depends(get_auth0_client)]
+    ):
+    """
+    Create a new role in Auth0 and add it to the DB.
+    Note that our "RoleId" is actually the
+    Auth0 role name - Auth0 has its own internal IDs
+    """
+    logger.info(f"Creating role {role_data.name} in Auth0")
+    resp = auth0_client.create_role(**role_data.model_dump())
+    logger.info("Saving to database")
+    role = Auth0Role(**resp.model_dump())
+    db_session.add(role)
+    db_session.commit()
+    return role
