63bb470d6df24550780d2f2a28dfafbe57f3c971
marius-mather <marius.mather@sydney.edu.au>
2025-07-22 16:12:12 +1000

    Test getting users by roles

--- tests/test_auth0_client.py
+++ tests/test_auth0_client.py
@@ -2,6 +2,7 @@ import pytest
 import respx
 from httpx import Response
 
+from auth0.client import UsersWithTotals
 from tests.datagen import BiocommonsAuth0UserFactory
 
 
@@ -73,3 +74,41 @@ def test_search_users_methods(auth0_client, method, query):
     assert request.url.params["page"] == "2"
     assert request.url.params["per_page"] == "50"
     assert result[0].model_dump(mode="json") == user.model_dump(mode="json")
+
+
+@respx.mock
+def test_get_role_users(auth0_client):
+    """
+    Test we can get users for a role from Auth0 API
+    """
+    role_id = "auth0|role_id"
+    users = BiocommonsAuth0UserFactory.batch(size=3)
+    route = respx.get(f"https://example.auth0.com/api/v2/roles/{role_id}/users").respond(
+        200, json=[u.model_dump(mode="json") for u in users]
+    )
+    result = auth0_client.get_role_users(role_id)
+    assert route.called
+    assert len(result) == 3
+    for user in result:
+        assert any(user.user_id == original.user_id for original in users)
+    assert result[0].model_dump(mode="json") == users[0].model_dump(mode="json")
+
+
+@respx.mock
+def test_get_all_role_users(auth0_client):
+    """
+    Test we can get all users for a role from Auth0 API, automatically
+    running through multiple pages if necessary.
+    """
+    role_id = "auth0|role_id"
+    users = BiocommonsAuth0UserFactory.batch(size=150)
+    batch1 = UsersWithTotals(users=users[:100], total=150, start=0, limit=100)
+    batch2 = UsersWithTotals(users=users[100:], total=150, start=100, limit=100)
+    route = respx.get(f"https://example.auth0.com/api/v2/roles/{role_id}/users").mock(
+        side_effect=[Response(200, json=batch1.model_dump(mode="json")),
+                     Response(200, json=batch2.model_dump(mode="json"))]
+    )
+    result = auth0_client.get_all_role_users(role_id)
+    assert route.called
+    assert route.call_count == 2
+    assert len(result) == 150
