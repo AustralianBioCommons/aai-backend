fa4cf6db93b5e175251ff000a56267b66829a0ad
marius-mather <marius.mather@sydney.edu.au>
2025-07-28 16:36:28 +1000

    Create database admin on app startup - means settings are not requested when the app is imported

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from contextlib import asynccontextmanager
 
 from dotenv import dotenv_values
 from fastapi import FastAPI
-from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
-from starlette.middleware.sessions import SessionMiddleware
 
 # This has to be imported even if unused
 from db import models  # noqa: F401
@@ -28,10 +26,10 @@ async def lifespan(app: FastAPI):
     # NOTE: we only create the database and tables automatically in development:
     # we assume that if the DB is an sqlite DB, we are in dev.
     create_db_and_tables()
+    DatabaseAdmin.setup(app=app, secret_key=SECRET_KEY)
     yield
 
-session_middleware = Middleware(SessionMiddleware, secret_key=SECRET_KEY, max_age=None)
-app = FastAPI(lifespan=lifespan, middleware=[session_middleware])
+app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
@@ -52,5 +50,3 @@ app.include_router(bpa_register.router)
 app.include_router(galaxy_register.router)
 app.include_router(utils.router)
 app.include_router(biocommons_groups.router)
-
-db_admin = DatabaseAdmin.setup(app=app, session_middleware=session_middleware)
