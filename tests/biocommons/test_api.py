5944dcd9ed3e1955c49acde357e344123cc153fd
marius-mather <marius.mather@sydney.edu.au>
2025-08-07 09:46:08 +1000

    Update unit tests of role creation

--- tests/biocommons/test_api.py
+++ tests/biocommons/test_api.py
@@ -60,11 +60,13 @@ def test_create_group(test_client, as_admin_user, override_auth0_client, test_db
 
 @pytest.mark.parametrize("role_name", ["biocommons/role/tsi/admin", "biocommons/group/tsi"])
 @respx.mock
-def test_create_role(role_name, test_client, as_admin_user, override_auth0_client, test_db_session):
+def test_create_role(role_name, test_client, as_admin_user, override_auth0_client, test_db_session, mocker):
     """
     Test we can create Auth0 roles using either the format for roles or groups.
     """
     mock_resp = RoleDataFactory.build(name=role_name)
+    # Patch check of existing role
+    mocker.patch("auth0.client.Auth0Client.get_role_by_name", side_effect=ValueError)
     route = respx.post("https://auth0.example.com/api/v2/roles").mock(
         return_value=Response(200, json=mock_resp.model_dump(mode="json"))
     )
@@ -77,6 +79,27 @@ def test_create_role(role_name, test_client, as_admin_user, override_auth0_clien
     )
     assert resp.status_code == 200
     assert route.called
+    role_from_db = test_db_session.exec(select(Auth0Role).where(Auth0Role.name == role_name)).one()
+    assert role_from_db.name == role_name
+
+
+@pytest.mark.parametrize("role_name", ["biocommons/role/tsi/admin", "biocommons/group/tsi"])
+def test_create_role_already_exists(role_name, test_client, as_admin_user, override_auth0_client, test_db_session, mocker):
+    """
+    Test we can add existing Auth0 roles to the DB
+    """
+    mock_resp = RoleDataFactory.build(name=role_name)
+    # Patch check of existing role
+    mocker.patch("auth0.client.Auth0Client.get_role_by_name", return_value=mock_resp)
+    # No call to Auth0 API to create when the role already exists
+    resp = test_client.post(
+        "/biocommons/roles/create",
+        json={
+            "name": role_name,
+            "description": "Admin role for Threatened Species Initiative"
+        }
+    )
+    assert resp.status_code == 200
     role_from_db = test_db_session.exec(select(Auth0Role).where(Auth0Role.name == role_name)).one()
     assert role_from_db.name == role_name
 
