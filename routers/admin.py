7d04a46817cd06f9187dd0c3975aa146505cfec5
marius-mather <marius.mather@sydney.edu.au>
2025-09-17 16:27:56 +1000

    Remove admin endpoints for service/resource approval

--- routers/admin.py
+++ routers/admin.py
@@ -1,4 +1,3 @@
-import asyncio
 import logging
 from datetime import datetime
 from typing import Annotated
@@ -9,7 +8,7 @@ from pydantic import BaseModel, Field, ValidationError
 from sqlalchemy import false, func, or_
 from sqlmodel import Session, select
 
-from auth.validator import get_current_user, user_is_admin
+from auth.validator import user_is_admin
 from auth0.client import Auth0Client, get_auth0_client
 from db.models import (
     BiocommonsGroup,
@@ -20,9 +19,7 @@ from db.models import (
 )
 from db.setup import get_db_session
 from db.types import ApprovalStatusEnum, GroupEnum
-from routers.user import update_user_metadata
 from schemas.biocommons import Auth0UserDataWithMemberships
-from schemas.user import SessionUser
 
 logger = logging.getLogger('uvicorn.error')
 
@@ -262,72 +259,3 @@ def resend_verification_email(user_id: Annotated[str, UserIdParam],
                               client: Annotated[Auth0Client, Depends(get_auth0_client)]):
     client.resend_verification_email(user_id)
     return {"message": "Verification email resent."}
-
-
-@router.post("/users/{user_id}/services/{service_id}/approve")
-def approve_service(user_id: Annotated[str, UserIdParam],
-                    service_id: Annotated[str, ServiceIdParam],
-                    client: Annotated[Auth0Client, Depends(get_auth0_client)],
-                    approving_user: Annotated[SessionUser, Depends(get_current_user)]):
-    user = client.get_user(user_id=user_id)
-    # Need to fetch full user info currently to get email address, not in access token
-    approving_user_data = client.get_user(user_id=approving_user.access_token.sub)
-    logger.debug(f"Approving service {service_id} for user {user_id} by {approving_user_data.email}")
-    user.app_metadata.approve_service(service_id, updated_by=str(approving_user_data.email))
-    logger.info("Sending updated metadata to Auth0 API")
-    # update_user_metadata is async, so run via asyncio
-    update = update_user_metadata(
-        user_id=user_id,
-        token=client.management_token,
-        metadata=user.app_metadata.model_dump(mode="json")
-    )
-    resp = asyncio.run(update)
-    logger.info("Metadata updated successfully")
-    return resp
-
-
-@router.post("/users/{user_id}/services/{service_id}/revoke")
-def revoke_service(user_id: Annotated[str, UserIdParam],
-                   service_id: Annotated[str, ServiceIdParam],
-                   client: Annotated[Auth0Client, Depends(get_auth0_client)],
-                   revoking_user: Annotated[SessionUser, Depends(get_current_user)]):
-    """
-    Revoke a service and all associated resources for a user.
-    """
-    user = client.get_user(user_id=user_id)
-    revoking_user_data = client.get_user(user_id=revoking_user.access_token.sub)
-    user.app_metadata.revoke_service(service_id=service_id, updated_by=str(revoking_user_data.email))
-    service = user.app_metadata.get_service_by_id(service_id)
-    for resource in service.resources:
-        resource.revoke()
-    update = update_user_metadata(
-        user_id=user_id,
-        token=client.management_token,
-        metadata=user.app_metadata.model_dump(mode="json")
-    )
-    resp = asyncio.run(update)
-    return resp
-
-
-@router.post("/users/{user_id}/services/{service_id}/resources/{resource_id}/approve")
-def approve_resource(user_id: Annotated[str, UserIdParam],
-                     service_id: Annotated[str, ServiceIdParam],
-                     resource_id: Annotated[str, ResourceIdParam],
-                     client: Annotated[Auth0Client, Depends(get_auth0_client)],
-                     approving_user: Annotated[SessionUser, Depends(get_current_user)]):
-    user = client.get_user(user_id=user_id)
-    approving_user_data = client.get_user(user_id=approving_user.access_token.sub)
-
-    user.app_metadata.approve_resource(
-        service_id=service_id,
-        resource_id=resource_id,
-        updated_by=approving_user_data.email
-    )
-
-    update = update_user_metadata(
-        user_id=user_id,
-        token=client.management_token,
-        metadata=user.app_metadata.model_dump(mode="json")
-    )
-    resp = asyncio.run(update)
-    return resp
