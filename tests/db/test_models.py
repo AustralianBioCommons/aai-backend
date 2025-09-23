f9264f38616a6daefac17f1dae953291739bd2df
marius-mather <marius.mather@sydney.edu.au>
2025-09-23 14:31:09 +1000

    Test platform IDs are unique

--- tests/db/test_models.py
+++ tests/db/test_models.py
@@ -111,6 +111,16 @@ def test_create_platform(platform_id, test_db_session, persistent_factories):
     assert platform.id == platform_id
 
 
+def test_create_platform_unique_id(test_db_session):
+    platform = Platform(id=PlatformEnum.GALAXY, name="Galaxy", admin_roles=[])
+    test_db_session.add(platform)
+    test_db_session.commit()
+    with pytest.raises(IntegrityError):
+        platform = Platform(id=PlatformEnum.GALAXY, name="Galaxy Duplicate", admin_roles=[])
+        test_db_session.add(platform)
+        test_db_session.commit()
+
+
 def test_create_platform_membership(test_db_session, persistent_factories, frozen_time):
     """
     Test creating a platform membership model
