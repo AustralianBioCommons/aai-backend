ff75d433cb7d2be7f5112b9317dc3f925e6ecafc
marius-mather <marius.mather@sydney.edu.au>
2025-10-09 12:01:03 +1100

    refactor: add DB checks to user_is_general_admin check

--- auth/validator.py
+++ auth/validator.py
@@ -6,8 +6,11 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwk, jwt
 from jose.exceptions import JWTError
+from sqlmodel import Session
 
 from config import Settings, get_settings
+from db.models import BiocommonsUser
+from db.setup import get_db_session
 from schemas.tokens import AccessTokenPayload
 from schemas.user import SessionUser
 
@@ -79,16 +82,31 @@ def get_rsa_key(token: str, settings: Settings, retry_on_failure: bool = True) -
     return None
 
 
-def get_current_user(
+def get_session_user(
     token: str = Depends(oauth2_scheme), settings: Settings = Depends(get_settings)
 ) -> SessionUser:
+    """
+    Get the current user's session data (access token).
+    """
     access_token = verify_jwt(token, settings=settings)
     return SessionUser(access_token=access_token)
 
 
+def get_db_user(
+    current_user: Annotated[SessionUser, Depends(get_session_user)],
+    db_session: Annotated[Session, Depends(get_db_session)], ) -> BiocommonsUser | None:
+    """
+    Get the user's DB record.
+    """
+    user = db_session.get(BiocommonsUser, current_user.access_token.sub)
+    return user
+
+
 def user_is_general_admin(
-    current_user: Annotated[SessionUser, Depends(get_current_user)],
+    current_user: Annotated[SessionUser, Depends(get_session_user)],
     settings: Annotated[Settings, Depends(get_settings)],
+    db_user: Annotated[BiocommonsUser, Depends(get_db_user)],
+    db_session: Annotated[Session, Depends(get_db_session)],
 ) -> SessionUser:
     """
     Check if user has general admin privileges.
@@ -98,9 +116,12 @@ def user_is_general_admin(
     """
     if current_user.is_biocommons_admin(settings=settings):
         return current_user
-    if not current_user.is_biocommons_admin(settings=settings):
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="You must be an admin to access this endpoint.",
-        )
-    return current_user
+    if db_user is not None:
+        if db_user.is_any_platform_admin(access_token=current_user.access_token, db_session=db_session):
+            return current_user
+        if db_user.is_any_group_admin(access_token=current_user.access_token, db_session=db_session):
+            return current_user
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="You must be an admin to access this endpoint.",
+    )
