cbce2f96d69483ebd2765c381173be8c2393efcc
marius-mather <marius.mather@sydney.edu.au>
2025-10-10 16:05:43 +1100

    refactor: rework group/role/platform creation endpoints

--- routers/biocommons_admin.py
+++ routers/biocommons_admin.py
@@ -14,7 +14,7 @@ from biocommons.groups import (
     GroupId,
     RoleId,
 )
-from db.models import Auth0Role
+from db.models import Auth0Role, Platform
 from db.setup import get_db_session
 from db.types import PlatformEnum
 
@@ -22,29 +22,26 @@ logger = logging.getLogger("uvicorn.error")
 
 # All routes should require biocommons admin permissions
 router = APIRouter(prefix="/biocommons-admin", tags=["admin"],
-                   dependencies=Depends(user_is_biocommons_admin))
+                   dependencies=[Depends(user_is_biocommons_admin)])
 
 
 @router.post("/groups/create",
              response_model=BiocommonsGroupResponse)
 def create_group(
         group_info: BiocommonsGroupCreate,
-        db_session: Annotated[Session, Depends(get_db_session)],
-        auth0_client: Annotated[Auth0Client, Depends(get_auth0_client)]):
+        db_session: Annotated[Session, Depends(get_db_session)],):
     """
     Create a new group in the DB. Note that the Auth0 role for this group
-    must already exist.
+    must already exist and be in the DB.
     """
-    # Check group exists in Auth0
-    try:
-        # Note: our "group ids" are actually the Auth0 role names
-        auth0_client.get_role_by_name(name=group_info.group_id)
-    except ValueError:
+    # Check group exists in the DB
+    existing_role = Auth0Role.get_by_name(group_info.group_id, db_session)
+    if existing_role is None:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND,
-            detail=f"Group {group_info.name} doesn't exist in Auth0"
+            detail=f"Role for {group_info.name} doesn't exist in the DB"
         )
-    group = group_info.save(session=db_session, auth0_client=auth0_client)
+    group = group_info.save_group(session=db_session)
     return BiocommonsGroupResponse(
         group_id=group.group_id,
         name=group.name,
@@ -57,14 +54,42 @@ class PlatformCreateData(BaseModel):
     name: str
     admin_roles: list[str]
 
-    def save(self, db_session: Session, auth0_client: Auth0Client):
+    def save_platform(self, db_session: Session, commit: bool = False):
+        """
+        Save the platform to the DB.
+        Any roles in admin_roles must already exist in the DB.
+        """
         for role in self.admin_roles:
-            Auth0Role.get_by_name(role, db_session)
+            db_role = Auth0Role.get_by_name(role, db_session)
+            if db_role is None:
+                raise HTTPException(
+                    status_code=HTTPStatus.BAD_REQUEST,
+                    detail=f"Role {role} doesn't exist in DB - create roles first"
+                )
+        platform = Platform(**self.model_dump())
+        db_session.add(platform)
+        if commit:
+            db_session.commit()
+        return platform
 
 
-@router.post("/platforms/create")
-def create_platform(platform_id: str, db_session: Annotated[Session, Depends(get_db_session)]):
-    pass
+class PlatformResponse(BaseModel):
+    id: PlatformEnum
+    name: str
+    admin_roles: list[Auth0Role]
+
+
+@router.post("/platforms/create",
+             response_model=PlatformResponse,)
+def create_platform(platform_data: PlatformCreateData, db_session: Annotated[Session, Depends(get_db_session)]):
+    existing = Platform.get_by_id(platform_data.id, db_session)
+    if existing is not None:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail=f"Platform {platform_data.id} already exists"
+        )
+    platform = platform_data.save_platform(db_session, commit=True)
+    return platform.model_dump()
 
 
 class CreateRoleData(BaseModel):
