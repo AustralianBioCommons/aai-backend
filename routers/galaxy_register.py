5caff675414342e20a1887a088205f12b13b3284
marius-mather <marius.mather@sydney.edu.au>
2025-10-17 14:20:46 +1100

    fix: remove Galaxy username check, not needed

--- routers/galaxy_register.py
+++ routers/galaxy_register.py
@@ -1,7 +1,6 @@
 import logging
 from typing import Annotated, Optional
 
-import httpx
 from fastapi import APIRouter, Header, HTTPException
 from fastapi.params import Depends
 from httpx import HTTPStatusError
@@ -12,13 +11,11 @@ from auth0.client import Auth0Client, get_auth0_client
 from config import Settings, get_settings
 from db.models import BiocommonsUser, PlatformEnum
 from db.setup import get_db_session
-from galaxy.client import GalaxyClient, get_galaxy_client
 from register.tokens import create_registration_token, verify_registration_token
 from routers.errors import RegistrationRoute
 from schemas.biocommons import Auth0UserData, BiocommonsRegisterData
 from schemas.galaxy import GalaxyRegistrationData
 from schemas.responses import (
-    FieldError,
     RegistrationErrorResponse,
     RegistrationResponse,
 )
@@ -47,7 +44,6 @@ async def get_registration_token(settings: Settings = Depends(get_settings)):
 def register(
         registration_data: GalaxyRegistrationData,
         settings: Annotated[Settings, Depends(get_settings)],
-        galaxy_client: Annotated[GalaxyClient, Depends(get_galaxy_client)],
         auth0_client: Annotated[Auth0Client, Depends(get_auth0_client)],
         db_session: Annotated[Session, Depends(get_db_session)],
         registration_token: Optional[str] = Header(None),
@@ -59,19 +55,6 @@ def register(
     logger.debug("Registration token verified.")
 
     user_data = BiocommonsRegisterData.from_galaxy_registration(registration_data)
-    logger.debug("Checking if username exists in Galaxy")
-    galaxy_username = user_data.username
-    try:
-        existing = galaxy_client.username_exists(galaxy_username)
-        if existing:
-            username_error = FieldError(field="username", message="Username already exists in Galaxy")
-            error_response = RegistrationErrorResponse(
-                message="Username already exists in Galaxy",
-                field_errors=[username_error]
-            )
-            return JSONResponse(status_code=400, content=error_response.model_dump(mode="json"))
-    except httpx.HTTPError as e:
-        logger.warning(f"Failed to check username in Galaxy: {e}")
 
     try:
         logger.info("Registering user with Auth0")
