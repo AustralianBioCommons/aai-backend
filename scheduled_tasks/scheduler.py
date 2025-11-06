08f8a57d7bea24a5a6051b26ec7009cf6ca3f9f6
marius-mather <marius.mather@sydney.edu.au>
2025-11-06 11:54:38 +1100

    fix: use a memory jobstore for sync tasks in dev, sqlite doesn't like async writes

--- scheduled_tasks/scheduler.py
+++ scheduled_tasks/scheduler.py
@@ -4,6 +4,7 @@ from apscheduler.events import (
     EVENT_JOB_MISSED,
     JobExecutionEvent,
 )
+from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from loguru import logger
@@ -22,9 +23,15 @@ def job_listener(event: JobExecutionEvent):
 def create_scheduler():
     from db.setup import get_db_config
     db_url, _ = get_db_config()
-    jobstores = {
-        "default": SQLAlchemyJobStore(url=db_url)
-    }
+    # For dev (with sqlite DB), just use MemoryJobStore - sqlite has issues with locking
+    if db_url.startswith("sqlite://"):
+        jobstores = {
+            "default": MemoryJobStore()
+        }
+    else:
+        jobstores = {
+            "default": SQLAlchemyJobStore(url=db_url)
+        }
     executors = {
         "default": {"type": "asyncio"},
     }
