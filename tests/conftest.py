61edfc5a2b5e9e2167250b27a7924c4025d92a37
marius-mather <marius.mather@sydney.edu.au>
2026-02-23 13:39:04 +1100

    test: switch to a file-based DB, much easier when testing rollbacks

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ from unittest.mock import MagicMock, patch
 import pytest
 from fastapi.testclient import TestClient
 from polyfactory import BaseFactory
-from sqlmodel import Session, StaticPool, create_engine
+from sqlmodel import Session, create_engine
 
 from auth.management import get_management_token
 from auth.ses import get_email_service
@@ -38,14 +38,14 @@ def suppress_third_party_warnings():
 
 
 @pytest.fixture(scope="function")
-def test_db_engine():
+def test_db_engine(tmp_path):
     from db import models  # noqa: F401
     from db.core import BaseModel
+
+    db_file = tmp_path / "test.sqlite"
     engine = create_engine(
-        # Use in-memory DB by default
-        "sqlite://",
+        f"sqlite:///{db_file}",
         connect_args={"check_same_thread": False},
-        poolclass=StaticPool,
     )
     BaseModel.metadata.create_all(engine)
     try:
@@ -56,16 +56,14 @@ def test_db_engine():
 
 @pytest.fixture(name="session")
 def session_fixture(test_db_engine):
-    connection = test_db_engine.connect()
-    transaction = connection.begin()
-    session = Session(bind=connection)
+    """
+    Session for the test thread (assertions/seeding).
+    """
+    session = Session(test_db_engine)
     try:
         yield session
     finally:
-        if transaction.is_active:
-            transaction.rollback()
         session.close()
-        connection.close()
 
 
 @pytest.fixture()
