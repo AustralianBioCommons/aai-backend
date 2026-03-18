8347f7237b1e02307f0e023e98236f3c3f1840e7
marius-mather <marius.mather@sydney.edu.au>
2026-03-18 16:29:16 +1100

    test: use mock settings in tests of email send tasks

--- tests/scheduled_tasks/test_tasks.py
+++ tests/scheduled_tasks/test_tasks.py
@@ -701,6 +701,7 @@ async def test_send_email_notification_retries_transient_errors(test_db_session,
     mock_service = mocker.Mock()
     mock_service.send.side_effect = EndpointConnectionError(endpoint_url="https://ses")
     mocker.patch("scheduled_tasks.tasks.get_email_service", return_value=mock_service)
+    mocker.patch("scheduled_tasks.tasks.get_settings", return_value=mock_settings)
     mocker.patch(
         "scheduled_tasks.tasks.get_db_session",
         return_value=_task_session_iter(test_db_session.get_bind()),
@@ -745,6 +746,7 @@ async def test_send_email_notification_does_not_retry_non_transient_error(test_d
     mock_service = mocker.Mock()
     mock_service.send.side_effect = error
     mocker.patch("scheduled_tasks.tasks.get_email_service", return_value=mock_service)
+    mocker.patch("scheduled_tasks.tasks.get_settings", return_value=mock_settings)
     mocker.patch(
         "scheduled_tasks.tasks.get_db_session",
         return_value=_task_session_iter(test_db_session.get_bind()),
