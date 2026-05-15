326bb96015ad2e6dd3ac1a4a592e9aedff21e0d5
Marius Mather <marius.mather@sydney.edu.au>
2026-05-15 10:14:44 +1000

    fix: SBP bundle should not be auto-approved

--- biocommons/bundles.py
+++ biocommons/bundles.py
@@ -68,8 +68,7 @@ BUNDLES: dict[BundleType, BiocommonsBundle] = {
     "sbp_workflow_execution": BiocommonsBundle(
         id="sbp_workflow_execution",
         group_id=GroupEnum.SBP,
-        # TODO: auto-approve based on domain?
-        group_auto_approve=True,
+        group_auto_approve=False,
         extra_platforms=[],
     )
 }
